425056ea53072e4400f84f2a5d78908982095433
Malfeasant <1212508+Malfeasant@users.noreply.github.com>
2023-12-17 23:42:26 -0700

    Reworked a bit New class for response, reduces clutter some.  Also, have to rewind buffer to read it.

--- src/main/java/us/malfeasant/ldapspoof/Listen.java
+++ src/main/java/us/malfeasant/ldapspoof/Listen.java
@@ -13,53 +13,40 @@ import org.tinylog.Logger;
 import javafx.beans.property.ReadOnlyIntegerProperty;
 import javafx.beans.property.ReadOnlyIntegerWrapper;
 
-public class Listen {
+public class Listen implements CompletionHandler<AsynchronousSocketChannel, Void> {
     private final ReadOnlyIntegerWrapper portWrapper;
     public final ReadOnlyIntegerProperty portProperty;
 
     private final AsynchronousServerSocketChannel channel;
 
     public Listen(int port) throws IOException {
-        // Port 0 means auto-select a random port, so we should have some way
-        // of getting that back to the UI...
         if (port < 0) throw new IllegalArgumentException("port must not be negative");
         portWrapper = new ReadOnlyIntegerWrapper(port);
         portProperty = portWrapper.getReadOnlyProperty();
 
         channel = AsynchronousServerSocketChannel.open().bind(
-            new InetSocketAddress(port));
-        channel.accept(null, new CompletionHandler<AsynchronousSocketChannel,Void>() {
-            @Override
-            public void completed(AsynchronousSocketChannel connection, Void att) {
-                // accept the next connection
-                channel.accept(null, this);
+            new InetSocketAddress(port)
+        );
+        // Port 0 means auto-select a port, so this is how we get it back to the UI...
+        // Interesting to note, this is a different InetSocketAddress than the one we
+        // passed in, we can't just read it back from the same object.
+        var actualPort = ((InetSocketAddress)channel.getLocalAddress()).getPort();
+        portWrapper.set(actualPort);
+        Logger.info("Listening on port {}", actualPort);
+        channel.accept(null, this);
+    }
 
-                try {
-                    Logger.info("Accepted connection from {}", connection.getRemoteAddress());
-                    var bb = ByteBuffer.allocate(0x1000);
-                    connection.read(bb, null, new CompletionHandler<Integer,Void>() {
-                        @Override
-                        public void completed(Integer result, Void attachment) {
-                            Logger.debug("Received {} bytes.", result);
-                            Logger.info("{}", StandardCharsets.UTF_8
-                                .decode(bb));   // but this just gets a blank line...
-                        }
-                        @Override
-                        public void failed(Throwable exc, Void attachment) {
-                            Logger.error("Problem acceptin response!\n{}", exc);
-                        }
-                    });
-                } catch (IOException e) {
-                    // Not sure what would cause this, just log for now.
-                    Logger.error("Problem reading channel!\n{}", e);
-                }
-            }
+    @Override
+    public void completed(AsynchronousSocketChannel connection, Void att) {
+        // accept the next connection
+        channel.accept(null, this);
 
-            @Override
-            public void failed(Throwable exc, Void attachment) {
-                // Not sure what would cause this, just log for now.
-                Logger.error("Problem accepting connection!\n{}", exc);
-            }
-        });
+        new Respond(connection);
+    }
+
+    @Override
+    public void failed(Throwable exc, Void attachment) {
+        // Not sure what would cause this, just log for now.
+        Logger.error("Problem accepting connection!\n{}", exc);
     }
 }
---
+++ src/main/java/us/malfeasant/ldapspoof/Respond.java
@@ -0,0 +1,37 @@
+package us.malfeasant.ldapspoof;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.nio.charset.StandardCharsets;
+
+import org.tinylog.Logger;
+
+public class Respond implements CompletionHandler<Integer, Void> {
+    private final ByteBuffer buffer;
+
+    public Respond(AsynchronousSocketChannel channel) {
+        buffer = ByteBuffer.allocate(0x1000);
+        try {
+            Logger.info("Accepted connection from {}", channel.getRemoteAddress());
+        } catch (IOException e) {
+            // Not sure what would cause this, just log for now.
+            Logger.error("Problem accepting connection!\n{}", e);
+        }
+        channel.read(buffer, null, this);
+    }
+
+    @Override
+    public void completed(Integer result, Void attachment) {
+        Logger.debug("Received {} bytes.", result);
+        buffer.rewind();
+        Logger.debug("{}", StandardCharsets.UTF_8.decode(buffer));
+    }
+
+    @Override
+    public void failed(Throwable e, Void attachment) {
+        Logger.error("Problem accepting response!\n{}", e);
+    }
+    
+}
