fd4f33585a6306e48aa6744c838a45577bf8d435
Malfeasant <1212508+Malfeasant@users.noreply.github.com>
2023-12-18 00:15:02 -0700

    Sends a response...

--- src/main/java/us/malfeasant/ldapspoof/Respond.java
+++ src/main/java/us/malfeasant/ldapspoof/Respond.java
@@ -10,8 +10,10 @@ import org.tinylog.Logger;
 
 public class Respond implements CompletionHandler<Integer, Void> {
     private final ByteBuffer buffer;
+    private final AsynchronousSocketChannel channel;
 
     public Respond(AsynchronousSocketChannel channel) {
+        this.channel = channel;
         buffer = ByteBuffer.allocate(0x1000);
         try {
             Logger.info("Accepted connection from {}", channel.getRemoteAddress());
@@ -27,11 +29,12 @@ public class Respond implements CompletionHandler<Integer, Void> {
         Logger.debug("Received {} bytes.", result);
         buffer.rewind();
         Logger.debug("{}", StandardCharsets.UTF_8.decode(buffer));
+        // 0 signifies success right?
+        channel.write(ByteBuffer.wrap("0\n".getBytes(StandardCharsets.UTF_8)));
     }
 
     @Override
     public void failed(Throwable e, Void attachment) {
         Logger.error("Problem accepting response!\n{}", e);
     }
-    
 }
